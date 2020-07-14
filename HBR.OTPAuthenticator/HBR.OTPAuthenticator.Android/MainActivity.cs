1efd2f8df5d2a3830e12c73093bab666364c85c3
JOscarSoft <jordy-dcna@hotmail.com>
2020-07-13 22:37:43 -0400

    Creating android splash screen

--- HBR.OTPAuthenticator/HBR.OTPAuthenticator.Android/MainActivity.cs
+++ HBR.OTPAuthenticator/HBR.OTPAuthenticator.Android/MainActivity.cs
@@ -12,7 +12,7 @@ using Plugin.CurrentActivity;
 
 namespace HBR.OTPAuthenticator.Droid
 {
-    [Activity(Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ HBR.OTPAuthenticator/HBR.OTPAuthenticator.Android/SplashActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HBR.OTPAuthenticator.Droid
+{
+    [Activity(MainLauncher = true, NoHistory = true, Theme = "@style/LaunchScreen.MainContainer")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SplashLayout);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { Startup(); });
+            startupWork.Start();
+        }
+
+        async void Startup()
+        {
+            await Task.Delay(1000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
